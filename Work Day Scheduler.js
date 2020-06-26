a34106d481b7774c4dbfee94cd9dff02f784aeb5
cherjios <slopez0286@gmail.com>
2020-06-25 19:08:11 -0700

    adding text chores

--- Work Day Scheduler.js
+++ Work Day Scheduler.js
@@ -3,6 +3,24 @@ $(document).ready(function(){
     //This function is to set todays day and time
     $("#todayIs").text(moment().format('LLLL'));
 
+    
+    
+
+    //Function to write chores to the box
+    $("#button9").on("click", function(event){
+        // event.preventDefault() prevents the form from trying to submit itself.
+        event.preventDefault();
+
+        var InputText = $("#chores").val().trim();
+
+       var Text9am = $("<p>").text(InputText);
+        $("#text9am").append(Text9am);
+
+        console.log(InputText);
+
+
+
+    });
 
 
 });
