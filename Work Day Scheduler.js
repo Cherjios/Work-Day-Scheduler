5c6300df1b9644fd33c048a2b7e6081b38c36649
cherjios <slopez0286@gmail.com>
2020-06-25 20:15:28 -0700

    addint 10 am

--- Work Day Scheduler.js
+++ Work Day Scheduler.js
@@ -7,19 +7,18 @@ $(document).ready(function(){
     
     
 
-    //Function to write chores to the box
+    //Function to write chores to the box 9 am
     $("#button9").on("click", function(event){
         // event.preventDefault() prevents the form from trying to submit itself.
         event.preventDefault();
 
-        var InputText = $("#chores").val().trim();
+        var InputText = $("#chores09").val().trim();
 
        var Text9am = $("<p>").text(InputText);
         $("#text9am").append(Text9am);
-        $("#chores").text("");
         console.log(InputText);
     });
-    //Change text color with the time 
+    //Change text color with the time 9 am
    if (currentTime === 9){
        $("#text9am").toggleClass("bg-success");
    }else if(currentTime < 9){
@@ -28,6 +27,25 @@ $(document).ready(function(){
     $("#text9am").toggleClass("bg-light");
    }
    
+   //Function to write chores to the box 10 am
+   $("#button10").on("click", function(event){
+    // event.preventDefault() prevents the form from trying to submit itself.
+    event.preventDefault();
+
+    var InputText = $("#chores10").val().trim();
+
+   var Text10am = $("<p>").text(InputText);
+    $("#text10am").append(Text10am);
+    console.log(InputText);
+});
+//Change text color with the time 10 am
+if (currentTime === 10){
+   $("#text10am").toggleClass("bg-success");
+}else if(currentTime < 10){
+$("#text10am").toggleClass("bg-danger");
+}else{
+$("#text10am").toggleClass("bg-light");
+}
 
 
 
